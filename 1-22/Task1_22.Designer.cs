806860aa362be3ba8d3980da94ad7cbfac3cf13a
Ok0lo <Ok0lo@mail.ru>
2023-09-09 14:09:47 +0700

    Task 1-22 Complete

---
+++ 1-22/Task1_22.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace _1_22 {
+    partial class Task1_22 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.Calculate_B = new System.Windows.Forms.Button();
+            this.Width_L = new System.Windows.Forms.Label();
+            this.Heght_L = new System.Windows.Forms.Label();
+            this.A_L = new System.Windows.Forms.Label();
+            this.B_L = new System.Windows.Forms.Label();
+            this.Result_L = new System.Windows.Forms.Label();
+            this.Result_TB = new System.Windows.Forms.TextBox();
+            this.Height_TB = new System.Windows.Forms.TextBox();
+            this.A_TB = new System.Windows.Forms.TextBox();
+            this.B_TB = new System.Windows.Forms.TextBox();
+            this.Width_TB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Calculate_B
+            // 
+            this.Calculate_B.Location = new System.Drawing.Point(182, 13);
+            this.Calculate_B.Margin = new System.Windows.Forms.Padding(5);
+            this.Calculate_B.Name = "Calculate_B";
+            this.Calculate_B.Size = new System.Drawing.Size(116, 46);
+            this.Calculate_B.TabIndex = 10;
+            this.Calculate_B.Text = "&Calculate";
+            this.Calculate_B.UseVisualStyleBackColor = true;
+            this.Calculate_B.Click += new System.EventHandler(this.Calculate_B_Click);
+            // 
+            // Width_L
+            // 
+            this.Width_L.AutoSize = true;
+            this.Width_L.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Width_L.Location = new System.Drawing.Point(14, 14);
+            this.Width_L.Margin = new System.Windows.Forms.Padding(5);
+            this.Width_L.Name = "Width_L";
+            this.Width_L.Size = new System.Drawing.Size(41, 16);
+            this.Width_L.TabIndex = 0;
+            this.Width_L.Text = "&Width";
+            // 
+            // Heght_L
+            // 
+            this.Heght_L.AutoSize = true;
+            this.Heght_L.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Heght_L.Location = new System.Drawing.Point(14, 40);
+            this.Heght_L.Margin = new System.Windows.Forms.Padding(5);
+            this.Heght_L.Name = "Heght_L";
+            this.Heght_L.Size = new System.Drawing.Size(46, 16);
+            this.Heght_L.TabIndex = 2;
+            this.Heght_L.Text = "&Height";
+            // 
+            // A_L
+            // 
+            this.A_L.AutoSize = true;
+            this.A_L.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.A_L.Location = new System.Drawing.Point(14, 66);
+            this.A_L.Margin = new System.Windows.Forms.Padding(5);
+            this.A_L.Name = "A_L";
+            this.A_L.Size = new System.Drawing.Size(16, 16);
+            this.A_L.TabIndex = 4;
+            this.A_L.Text = "&A";
+            // 
+            // B_L
+            // 
+            this.B_L.AutoSize = true;
+            this.B_L.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.B_L.Location = new System.Drawing.Point(14, 92);
+            this.B_L.Margin = new System.Windows.Forms.Padding(5);
+            this.B_L.Name = "B_L";
+            this.B_L.Size = new System.Drawing.Size(16, 16);
+            this.B_L.TabIndex = 6;
+            this.B_L.Text = "&B";
+            // 
+            // Result_L
+            // 
+            this.Result_L.AutoSize = true;
+            this.Result_L.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Result_L.Location = new System.Drawing.Point(14, 132);
+            this.Result_L.Margin = new System.Windows.Forms.Padding(5);
+            this.Result_L.Name = "Result_L";
+            this.Result_L.Size = new System.Drawing.Size(45, 16);
+            this.Result_L.TabIndex = 8;
+            this.Result_L.Text = "&Result";
+            // 
+            // Result_TB
+            // 
+            this.Result_TB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Result_TB.Location = new System.Drawing.Point(65, 129);
+            this.Result_TB.Margin = new System.Windows.Forms.Padding(5);
+            this.Result_TB.Name = "Result_TB";
+            this.Result_TB.ReadOnly = true;
+            this.Result_TB.Size = new System.Drawing.Size(233, 22);
+            this.Result_TB.TabIndex = 9;
+            // 
+            // Height_TB
+            // 
+            this.Height_TB.Location = new System.Drawing.Point(65, 39);
+            this.Height_TB.Margin = new System.Windows.Forms.Padding(5);
+            this.Height_TB.Name = "Height_TB";
+            this.Height_TB.Size = new System.Drawing.Size(100, 20);
+            this.Height_TB.TabIndex = 3;
+            // 
+            // A_TB
+            // 
+            this.A_TB.Location = new System.Drawing.Point(65, 65);
+            this.A_TB.Margin = new System.Windows.Forms.Padding(5);
+            this.A_TB.Name = "A_TB";
+            this.A_TB.Size = new System.Drawing.Size(100, 20);
+            this.A_TB.TabIndex = 5;
+            // 
+            // B_TB
+            // 
+            this.B_TB.Location = new System.Drawing.Point(65, 91);
+            this.B_TB.Margin = new System.Windows.Forms.Padding(5);
+            this.B_TB.Name = "B_TB";
+            this.B_TB.Size = new System.Drawing.Size(100, 20);
+            this.B_TB.TabIndex = 7;
+            // 
+            // Width_TB
+            // 
+            this.Width_TB.Location = new System.Drawing.Point(65, 13);
+            this.Width_TB.Margin = new System.Windows.Forms.Padding(5);
+            this.Width_TB.Name = "Width_TB";
+            this.Width_TB.Size = new System.Drawing.Size(100, 20);
+            this.Width_TB.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(179, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 52);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "*Restrictions\r\nWidth >= A, B\r\nA != B\r\nA, B, Width, Height != 0\r\n";
+            // 
+            // Task1_22
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 169);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Calculate_B);
+            this.Controls.Add(this.Result_TB);
+            this.Controls.Add(this.Result_L);
+            this.Controls.Add(this.B_TB);
+            this.Controls.Add(this.B_L);
+            this.Controls.Add(this.A_TB);
+            this.Controls.Add(this.A_L);
+            this.Controls.Add(this.Height_TB);
+            this.Controls.Add(this.Heght_L);
+            this.Controls.Add(this.Width_TB);
+            this.Controls.Add(this.Width_L);
+            this.Name = "Task1_22";
+            this.Text = "Task 1-22";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Width_L;
+        private System.Windows.Forms.Label Heght_L;
+        private System.Windows.Forms.Label A_L;
+        private System.Windows.Forms.Label B_L;
+        private System.Windows.Forms.Label Result_L;
+        private System.Windows.Forms.TextBox Result_TB;
+        private System.Windows.Forms.TextBox Height_TB;
+        private System.Windows.Forms.TextBox A_TB;
+        private System.Windows.Forms.TextBox B_TB;
+        private System.Windows.Forms.TextBox Width_TB;
+        private System.Windows.Forms.Button Calculate_B;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ 1-22/Task1_22.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _1_22 {
+
+    public partial class Task1_22 : Form {
+
+        public Task1_22() {
+            InitializeComponent();
+        }
+
+        private void Calculate_B_Click(object sender, EventArgs e) {
+            if (IsAllTextBoxesFilled() == false) {
+                Result_TB.Text = "Fill in all Text Boxes!";
+                return;
+            }
+
+            int width = Convert.ToInt32(Width_TB.Text);
+            int height = Convert.ToInt32(Height_TB.Text);
+            int a = Convert.ToInt32(A_TB.Text);
+            int b = Convert.ToInt32(B_TB.Text);
+
+            float rectangleSquare = width * height;
+            float trapezoidSquare = ((a + b) / 2f) * 2f;
+
+            Result_TB.Text = $"{trapezoidSquare / rectangleSquare}";
+        }
+
+        private bool IsAllTextBoxesFilled() {
+            if (Width_TB.Text.Length == 0) return false;
+            if (Height_TB.Text.Length == 0) return false;
+            if (A_TB.Text.Length == 0) return false;
+            if (B_TB.Text.Length == 0) return false;
+
+            return true;
+        }
+
+    }
+
+}
