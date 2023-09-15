3fb69fab026fd009f099dc0b636b86e4b50f0641
Ok0lo <Ok0lo@mail.ru>
2023-09-15 13:53:57 +0700

    Complete Task 2-22

---
+++ 2-22/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace _2_22 {
+    partial class Form1 {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            this.L_TB = new System.Windows.Forms.TextBox();
+            this.L_L = new System.Windows.Forms.Label();
+            this.P_TB = new System.Windows.Forms.TextBox();
+            this.P_L = new System.Windows.Forms.Label();
+            this.K_TB = new System.Windows.Forms.TextBox();
+            this.K_L = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Y_TB = new System.Windows.Forms.TextBox();
+            this.Y_L = new System.Windows.Forms.Label();
+            this.Calculate_B = new System.Windows.Forms.Button();
+            this.X_TB = new System.Windows.Forms.TextBox();
+            this.X_L = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // L_TB
+            // 
+            this.L_TB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.L_TB.Location = new System.Drawing.Point(41, 129);
+            this.L_TB.Margin = new System.Windows.Forms.Padding(5);
+            this.L_TB.Name = "L_TB";
+            this.L_TB.ReadOnly = true;
+            this.L_TB.Size = new System.Drawing.Size(130, 22);
+            this.L_TB.TabIndex = 52;
+            this.L_TB.TabStop = false;
+            // 
+            // L_L
+            // 
+            this.L_L.AutoSize = true;
+            this.L_L.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.L_L.Location = new System.Drawing.Point(13, 132);
+            this.L_L.Margin = new System.Windows.Forms.Padding(5);
+            this.L_L.Name = "L_L";
+            this.L_L.Size = new System.Drawing.Size(10, 16);
+            this.L_L.TabIndex = 51;
+            this.L_L.Text = "l";
+            // 
+            // P_TB
+            // 
+            this.P_TB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.P_TB.Location = new System.Drawing.Point(42, 97);
+            this.P_TB.Margin = new System.Windows.Forms.Padding(5);
+            this.P_TB.Name = "P_TB";
+            this.P_TB.ReadOnly = true;
+            this.P_TB.Size = new System.Drawing.Size(130, 22);
+            this.P_TB.TabIndex = 50;
+            this.P_TB.TabStop = false;
+            // 
+            // P_L
+            // 
+            this.P_L.AutoSize = true;
+            this.P_L.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.P_L.Location = new System.Drawing.Point(14, 100);
+            this.P_L.Margin = new System.Windows.Forms.Padding(5);
+            this.P_L.Name = "P_L";
+            this.P_L.Size = new System.Drawing.Size(15, 16);
+            this.P_L.TabIndex = 49;
+            this.P_L.Text = "p";
+            // 
+            // K_TB
+            // 
+            this.K_TB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.K_TB.Location = new System.Drawing.Point(41, 68);
+            this.K_TB.Margin = new System.Windows.Forms.Padding(20);
+            this.K_TB.Name = "K_TB";
+            this.K_TB.ReadOnly = true;
+            this.K_TB.Size = new System.Drawing.Size(130, 22);
+            this.K_TB.TabIndex = 48;
+            this.K_TB.TabStop = false;
+            // 
+            // K_L
+            // 
+            this.K_L.AutoSize = true;
+            this.K_L.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.K_L.Location = new System.Drawing.Point(13, 71);
+            this.K_L.Margin = new System.Windows.Forms.Padding(20, 20, 20, 8);
+            this.K_L.Name = "K_L";
+            this.K_L.Size = new System.Drawing.Size(14, 16);
+            this.K_L.TabIndex = 47;
+            this.K_L.Text = "k";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(185, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 26);
+            this.label1.TabIndex = 45;
+            this.label1.Text = "*Conditions\r\nX, Y > 0\r\n";
+            // 
+            // Y_TB
+            // 
+            this.Y_TB.Location = new System.Drawing.Point(42, 39);
+            this.Y_TB.Margin = new System.Windows.Forms.Padding(5);
+            this.Y_TB.Name = "Y_TB";
+            this.Y_TB.Size = new System.Drawing.Size(129, 20);
+            this.Y_TB.TabIndex = 44;
+            // 
+            // Y_L
+            // 
+            this.Y_L.AutoSize = true;
+            this.Y_L.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Y_L.Location = new System.Drawing.Point(14, 40);
+            this.Y_L.Margin = new System.Windows.Forms.Padding(5);
+            this.Y_L.Name = "Y_L";
+            this.Y_L.Size = new System.Drawing.Size(16, 16);
+            this.Y_L.TabIndex = 43;
+            this.Y_L.Text = "&Y";
+            // 
+            // Calculate_B
+            // 
+            this.Calculate_B.Location = new System.Drawing.Point(188, 13);
+            this.Calculate_B.Margin = new System.Windows.Forms.Padding(5);
+            this.Calculate_B.Name = "Calculate_B";
+            this.Calculate_B.Size = new System.Drawing.Size(116, 46);
+            this.Calculate_B.TabIndex = 46;
+            this.Calculate_B.Text = "&Calculate";
+            this.Calculate_B.UseVisualStyleBackColor = true;
+            this.Calculate_B.Click += new System.EventHandler(this.Calculate_B_Click);
+            // 
+            // X_TB
+            // 
+            this.X_TB.Location = new System.Drawing.Point(42, 13);
+            this.X_TB.Margin = new System.Windows.Forms.Padding(5);
+            this.X_TB.Name = "X_TB";
+            this.X_TB.Size = new System.Drawing.Size(129, 20);
+            this.X_TB.TabIndex = 42;
+            // 
+            // X_L
+            // 
+            this.X_L.AutoSize = true;
+            this.X_L.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.X_L.Location = new System.Drawing.Point(14, 14);
+            this.X_L.Margin = new System.Windows.Forms.Padding(5);
+            this.X_L.Name = "X_L";
+            this.X_L.Size = new System.Drawing.Size(15, 16);
+            this.X_L.TabIndex = 41;
+            this.X_L.Text = "&X";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 165);
+            this.Controls.Add(this.L_TB);
+            this.Controls.Add(this.L_L);
+            this.Controls.Add(this.P_TB);
+            this.Controls.Add(this.P_L);
+            this.Controls.Add(this.K_TB);
+            this.Controls.Add(this.K_L);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Y_TB);
+            this.Controls.Add(this.Y_L);
+            this.Controls.Add(this.Calculate_B);
+            this.Controls.Add(this.X_TB);
+            this.Controls.Add(this.X_L);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Task 2-22";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox L_TB;
+        private System.Windows.Forms.Label L_L;
+        private System.Windows.Forms.TextBox P_TB;
+        private System.Windows.Forms.Label P_L;
+        private System.Windows.Forms.TextBox K_TB;
+        private System.Windows.Forms.Label K_L;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Y_TB;
+        private System.Windows.Forms.Label Y_L;
+        private System.Windows.Forms.Button Calculate_B;
+        private System.Windows.Forms.TextBox X_TB;
+        private System.Windows.Forms.Label X_L;
+    }
+}
+
---
+++ 2-22/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _2_22 {
+
+    public partial class Form1 : Form {
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Calculate_B_Click(object sender, EventArgs e) {
+            double x = Convert.ToDouble(X_TB.Text);
+            double y = Convert.ToDouble(Y_TB.Text);
+
+            double k = Math.Pow(0.273f * Math.Log10(x), 1.573f * Math.Log10(y));
+            double p = Math.Tan(Math.Log10(Math.Pow(Math.Log10(y / x), 1f / 7f)));
+            double l = CalculateL(k, p);
+
+            K_TB.Text = k.ToString();
+            P_TB.Text = p.ToString();
+            L_TB.Text = l.ToString();
+        }
+
+        private double CalculateL(double k, double p) {
+            if (k < Math.Abs(p)) {
+                return Math.Abs(k - p);
+            }
+
+            return (7f * k - 5f * p) / (2f * (k * k) + 3f * (p * p));
+        }
+
+    }
+
+}
